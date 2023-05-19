ef6672d6e15d6055db2155c4b5fd008f448ba633
Rayne <raynebaby404@gmail.com>
2023-05-19 19:59:36 -0400

    updated from Padre's

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+var input1;
+var input2;
+
+
+
 function button() {
-    alert("Whore.")
+    alert("Whore.");
 }
 
 function buttontwo() {
-    alert("Did it work?")
+    alert("Did it work?");
+}
+
+function changevalues() {
+    input1 = Number(document.getElementById("number1").value);
+    input2 = Number(document.getElementById("number2").value);
+    document.getElementById("output").innerHTML = sum(input1,input2);
+}
+
+function sum(number1, number2) {
+    var todd = number1 + number2;
+    return todd
+
 }
